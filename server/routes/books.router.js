e64c6d41943df43956a8b299e21a2256880a6aed
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2023-01-03 19:31:23 -0600

    still setting up route

--- server/routes/books.router.js
+++ server/routes/books.router.js
@@ -1,3 +1,21 @@
 const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
+
+// getting all books from database
+router.get("/", (req, res) => {
+    const query = `
+        SELECT * 
+        FROM books
+        ORDER BY "title" ASC;
+      `;
+    pool
+      .query(query)
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((err) => {
+        console.log("ERROR: getting books", err);
+        res.sendStatus(500);
+      });
+  });
