dc46f3f3e42defd936caf7cf59e2059e3c491dd6
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2023-01-03 20:02:14 -0600

    search router

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -3,3 +3,26 @@ const pool = require("../modules/pool");
 const router = express.Router();
 const axios = require("axios");
 require("dotenv").config();
+
+// will be used to grab search history from api 
+router.get("/:search", (req, res) => {
+  const search = req.params.search;
+  console.log("/search books");
+  axios({
+    method: "GET",
+    url: `https://www.googleapis.com/books/v1/volumes?q=${search}`,
+params: {
+          api_key: process.env.SERVER_SESSION_SECRET,
+          limit: 1,
+        },
+      })
+        .then((response) => {
+          console.log("axios response", response.data);
+          res.send(response.data.items);
+        }).catch((err) => {
+              console.log("error getting books from search", err)
+              res.sendStatus(500);
+            });
+        });
+        
+        module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const booksRouter = require('./routes/books.router');
+const searchRouter= require('./routes/search.router')
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,6 +25,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 // app.use('api/books',booksRouter)
+app.use ('/api/search',searchRouter)
 
 // Serve static files
 app.use(express.static('build'));
