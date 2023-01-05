62ccc58cd7b323b2acade6490b052a2a4caf8b0a
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2023-01-04 19:16:07 -0600

    setting up search routes

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {useDispatch} from 'react-redux';
+import {useState} from 'react';
+import {useHistory} from 'react-router-dom'
+import axios from 'axios';
+
