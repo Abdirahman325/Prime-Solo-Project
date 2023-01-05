f96074ca46fb3c01af88018de2926a64a1582c1a
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2023-01-04 20:01:20 -0600

    set up for description function

---
+++ src/components/DescriptionPage/DescriptionPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
+function descriptionPage(){
+
+    return({
+        
+    })
+
+} export default descriptionPage;
